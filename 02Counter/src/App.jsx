7732f91316cef3c2f2f787ee9eae91cbb3afcb7b
deep9685 <deepaknmdv14@gmail.com>
2024-09-22 15:43:07 +0530

    learn about hooks and state

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  const increaseValue = () => {
+    setCount(count+1);
+  }
+
+  const decreaseValue = () => {
+    setCount(count-1);
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value : {count}</h2>
+
+      <div style={{display:"flex", justifyContent:"center", gap:"15px"}}>
+        <button onClick={increaseValue}>Increase</button>
+        <button onClick={decreaseValue}>Decrease</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
